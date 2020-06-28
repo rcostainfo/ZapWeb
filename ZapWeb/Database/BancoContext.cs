26d85710b7d2482a4d3b2f405c5da79c449bbd9c
Robson Costa <rcostainfo@gmail.com>
2020-06-28 16:10:43 +0100

    Criação do projeto

---
+++ ZapWeb/Database/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZapWeb.Database
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
